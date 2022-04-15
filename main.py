01bfebee54258c92434d868375b9345da5704026
avouacr <avouacr@pm.me>
2022-04-15 15:34:35 +0000

    Revert "config file for minio creds"
    This reverts commit 5bcd0540a123cdf7a0b646f1371bb2314628a36e.

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-"""Main script of the project."""
-import yaml
 
 from src.data.import_data import import_clean_data
 from src.data.train_test_split import make_val_split
@@ -8,12 +6,6 @@ from src.models.train_evaluate import evaluate_rdmf
 
 
 if __name__ == "__main__":
-
-    with open("config.yml", 'r') as file_in:
-        config = yaml.safe_load(file_in)
-    bucket = config['minio']['bucket']
-    location = config['minio']['path']
-
     training_data, test_data = import_clean_data("avouacr", "diffusion/ensae-reproductibilite")
 
     mean_age = round(training_data['Age'].mean())
