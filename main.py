161f500d617d395e27ee924998ef719f9aa9665b
avouacr <avouacr@pm.me>
2022-04-15 15:43:40 +0000

    s3 portable using urls and config file

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+"""Main script of the project."""
+import yaml
+import pandas as pd
 
-from src.data.import_data import import_clean_data
 from src.data.train_test_split import make_val_split
 from src.features.build_features import feature_engineering, label_encode_variable
 from src.models.train_evaluate import evaluate_rdmf
 
 
 if __name__ == "__main__":
-    training_data, test_data = import_clean_data("avouacr", "diffusion/ensae-reproductibilite")
+
+    with open("config.yml", 'r') as stream:
+        config = yaml.safe_load(stream)
+    training_data = pd.read_csv(config["minio-urls"]["train"])
+    test_data = pd.read_csv(config["minio-urls"]["test"])
 
     mean_age = round(training_data['Age'].mean())
     training_data = feature_engineering(training_data, mean_age)
